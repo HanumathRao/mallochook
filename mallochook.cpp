5da5ee764e66169c64c27f710e81805af07f9fc8
Lorenzo Pistone <blaffablaffa@gmail.com>
2011-12-31 14:12:27 +0100

    prettiness

--- mallochook.cpp
+++ mallochook.cpp
@@ -24,37 +24,37 @@ typedef boost::unordered_map<void*, memoryblock> boostmap;
 
 static void* dumpmemoryinfo(void*);
 
-static __thread bool inhook=false;
-static pthread_mutex_t mutex;
-static pthread_cond_t doyourjob;
-static void* dumpmemoryinfo(void*);
-static uint64_t start;
-
-static void wakedumper(int){
-	pthread_cond_signal(&doyourjob);
-}
-
 static uint64_t gettimemillis(){
 	timeval t;
 	gettimeofday(&t, 0);
 	return uint64_t(t.tv_sec)*1000L+(t.tv_usec/1000L);
 }
 
+static __thread bool inhook=false;
+static uint64_t start;
+static boostmap* volatile memoryinfo;
+static pthread_mutex_t mutex;
+static pthread_cond_t doyourjob;
+
+static void wakedumper(int){
+	pthread_cond_signal(&doyourjob);
+}
+
+static void* dumpmemoryinfo(void*);
+
+static int init() __attribute__((constructor));
 static int init(){
+	inhook=true;
+	start=gettimemillis();
+	memoryinfo=new boostmap();
 	pthread_mutex_init(&mutex, 0);
 	pthread_cond_init(&doyourjob, 0);
 	pthread_t id;
 	pthread_create(&id, 0, dumpmemoryinfo, 0);
 	signal(SIGUSR1, wakedumper);
-	start=gettimemillis();
 	cout<<"libmallokhook loaded"<<endl;
+	inhook=false;
 	return 0;
-}
-
-static boostmap& getmap(){
-	static boostmap memoryinfo;
-	static volatile int inithook=init();
-	return memoryinfo;
 }
 
 extern "C" void* __libc_malloc(size_t);
@@ -62,13 +62,12 @@ extern "C" void* malloc(size_t size){
 	void* result=__libc_malloc(size);
 	if(inhook) return result;
 	inhook=true;
-	boostmap& memoryinfo=getmap();
 
 	memoryblock info(size);
 	info.alloc_time=gettimemillis();
 	info.stackdepth=backtrace(info.stack, DEPTH);
 	pthread_mutex_lock(&mutex);
-	memoryinfo[result]=info;
+	(*memoryinfo)[result]=info;
 	pthread_mutex_unlock(&mutex);
 
 	inhook=false;
@@ -80,13 +79,12 @@ extern "C" void* realloc(void* ptr, size_t size){
 	void* result=__libc_realloc(ptr, size);
 	if(inhook) return result;
 	inhook=true;
-	boostmap& memoryinfo=getmap();
 
 	pthread_mutex_lock(&mutex);
-	boostmap::iterator it=memoryinfo.find(ptr);
-	if(it!=memoryinfo.end()){
-		(memoryinfo[result]=it->second).reallocated=true;
-		memoryinfo.erase(it);
+	boostmap::iterator it=memoryinfo->find(ptr);
+	if(it!=memoryinfo->end()){
+		((*memoryinfo)[result]=it->second).reallocated=true;
+		memoryinfo->erase(it);
 	}
 	pthread_mutex_unlock(&mutex);
 
@@ -99,11 +97,10 @@ extern "C" void free(void* ptr){
 	__libc_free(ptr);
 	if(inhook) return;
 	inhook=true;
-	boostmap& memoryinfo=getmap();
 
 	pthread_mutex_lock(&mutex);
-	boostmap::iterator it=memoryinfo.find(ptr);
-	if(it!=memoryinfo.end()) memoryinfo.erase(it);
+	boostmap::iterator it=memoryinfo->find(ptr);
+	if(it!=memoryinfo->end()) memoryinfo->erase(it);
 	pthread_mutex_unlock(&mutex);
 
 	inhook=false;
@@ -114,7 +111,6 @@ static void* dumpmemoryinfo(void*){
 		pthread_mutex_lock(&mutex);
 		pthread_cond_wait(&doyourjob, &mutex);
 		inhook=true;
-		boostmap& memoryinfo=getmap();
 
 		stringstream filename;
 		static int uniquifier=0;
@@ -127,8 +123,8 @@ static void* dumpmemoryinfo(void*){
 		filename<<"memoryinfo_"<<nowclock-start<<'_'<<uniquifier++;
 		ofstream out(filename.str().c_str());
 		out<<"memoryinforaw={"<<endl;
-		for(boostmap::iterator it=memoryinfo.begin(); it!=memoryinfo.end(); it++){
-			if(it!=memoryinfo.begin()) out<<","<<endl;
+		for(boostmap::iterator it=memoryinfo->begin(); it!=memoryinfo->end(); it++){
+			if(it!=memoryinfo->begin()) out<<","<<endl;
 			memoryblock& bl=it->second;
 			out<<"{"<<(bl.reallocated?"True":"False")<<", "<<bl.bytes<<", {";
 			for(size_t i=0; i<bl.stackdepth; i++) out<<(i?", ":"")<<(unsigned long int)(bl.stack[i]);
